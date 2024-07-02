464284603bcc86e800f527e067a2ba3eebbbf3e0
aditir360 <71522856+aditir360@users.noreply.github.com>
2024-07-01 17:23:13 -0700

    Update multiplication_app.py

--- multiplication_app.py
+++ multiplication_app.py
@@ -61,6 +61,78 @@ def application(environ, start_response):
         [un, pw] = cookies['session'].value.split(':')
         user = cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', [un, pw]).fetchall()
 
+        # This is where the game begins. This section of is code only executed if the login form works,
+        # and if the user is successfully logged in.
+        if user:
+            correct = 0
+            wrong = 0
+
+            cookies = http.cookies.SimpleCookie()
+            if 'HTTP_COOKIE' in environ:
+                cookies.load(environ['HTTP_COOKIE'])
+                if 'correct' in cookies:
+                    correct = int(cookies['correct'].value)
+                if 'wrong' in cookies:
+                    wrong = int(cookies['wrong'].value)
+                # [INSERT CODE FOR COOKIES HERE]
+
+            page = '<!DOCTYPE html><html><head><title>Multiply with Score</title></head><body>'
+            if 'factor1' in params and 'factor2' in params and 'answer' in params:
+                if (int(params['factor1'][0]) * int(params['factor2'][0])) == int(params['answer'][0]):
+                    page += '<head><h1 style="background-color: lightgreen">Correct, {} x {} = {}</h1></head>'.format(params['factor1'][0], params['factor2'][0], params['answer'][0])
+                    page += '<a href="/account">Back to multiplication game! --> </a>'
+                    correct += 1
+                    headers += [('Set-Cookie', 'correct={}'.format(correct))]
+
+                else:
+                    page += '<head><h1 style="background-color: red">Incorrect, {} x {} = {}</h1></head>'.format(params['factor1'][0], params['factor2'][0], int(params['factor1'][0]) * int(params['factor2'][0]))
+                    page += '<a href="/account">Back to multiplication game! --> </a>'
+                    wrong += 1
+                    headers += [('Set-Cookie', 'wrong={}'.format(wrong))]
+
+                return [page.encode()]
+
+            elif 'reset' in params:
+                correct = 0
+                wrong = 0
+
+            headers.append(('Set-Cookie', 'correct={}'.format(correct)))
+            headers.append(('Set-Cookie', 'wrong={}'.format(wrong)))
+
+            f1 = random.randrange(10) + 1
+            f2 = random.randrange(10) + 1
+
+            page = page + '<h1>What is {} x {}?</h1>'.format(f1, f2)
+
+            answer = [f1*f2, random.randrange(f2+10, 100), (f1*f2) + 5, random.randrange(70, 100)]
+            random.shuffle(answer)
+
+            hyperlink = '<a href="/account?username={}&amp;password={}&amp;factor1={}&amp;factor2={}&amp;answer={}">{}</a><br>'
+
+            ans_1 = hyperlink.format(un, pw, f1, f2, answer[0], answer[0])
+            ans_2 = hyperlink.format(un, pw, f1, f2, answer[1], answer[1])
+            ans_3 = hyperlink.format(un, pw, f1, f2, answer[2], answer[2])
+            ans_4 = hyperlink.format(un, pw, f1, f2, answer[3], answer[3])
+
+            page += '''
+            A: {}<br>
+            B: {}<br>
+            C: {}<br>
+            D: {}<br>
+            <br>
+            '''.format(ans_1, ans_2, ans_3, ans_4)
+
+
+            page += '''<h2>Score</h2>
+            Correct: {}<br>
+            Wrong: {}<br>
+            <a href="/account?reset=true">Reset</a>
+            </body></html>'''.format(correct, wrong)
+
+            return [page.encode()]
+        else:
+            return ['Not logged in. <a href="/">Login</a>'.encode()]
+
     elif path == '/':
         start_response('200 OK', headers)
         page = '''
